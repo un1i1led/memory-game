77407c4c507b1889c1e61950ca8e3c5697e21f6a
gil <gsqwow@gmail.com>
2023-02-23 02:48:03 -0700

    Add pokemon imgs

---
+++ src/img/index.js
@@ -0,0 +1,10 @@
+import charmander from './charmander.png';
+import lunala from './lunala.png';
+import hakamo from './hakamo-o.png';
+import sandshrew from './sandshrew.png';
+import ninjask from './ninjask.png';
+import tympole from './tympole.png';
+import togekiss from './togekiss.png';
+import lickitung from './lickitung.png';
+
+export {charmander, lunala, hakamo, sandshrew, ninjask, tympole, togekiss, lickitung};
