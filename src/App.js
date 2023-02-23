f02cd9b117bca3e5b12dbac34331d5578dfd2535
gil <gsqwow@gmail.com>
2023-02-22 23:44:09 -0700

    Fix updateState to correctly set score

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import React, { useState } from 'react';
+import GameContainer from './components/GameContainer';
 import TopBar from './components/TopBar';
+import './style.css';
 
 const App = () => {
   const [bestScore, setBestScore] = useState(0);
   const [score, setScore] = useState(0);
-  const [pokeArray, setPokeArray] = useState([]);
+  
 
   const updateState = (state, value) => {
     if (state === 'score') {
-      setScore(score + 1);
-      checkBestScore();
-    } else if (state === 'pokeArray') {
-      setPokeArray(
-        [
-          ...pokeArray, value
-        ]
-      );
-    }
+      setScore(value);
+      if (bestScore <= value) {
+        setBestScore(value);
+      }
+    } 
   };
-
-  const checkBestScore = () => {
-    if (score > bestScore) {
-      setBestScore(score);
-    }
-  }
 
   return (
     <div className='App'>
       <TopBar score={score} bestScore={bestScore}/>
+      <GameContainer score={score} updateState={updateState}/>
     </div>
   )
 }
