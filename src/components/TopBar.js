b8bef9f4ae3e4a219fcfc4bd1125800959eb2e15
gil <gsqwow@gmail.com>
2023-02-22 23:46:14 -0700

    Add separation of scores with divs

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const TopBar = props => {
     return (
-        <div>
-            {props.bestScore}
-            {props.score}
+        <div className='top-bar'>
+            Memory Game
+            <div>
+                Best score: {props.bestScore}
+            </div>
+            <div>
+                Score: {props.score}
+            </div>
         </div>
     )
 }
