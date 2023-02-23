5d8145a317b1b4c50cf6e380ca41071ac981ff02
gil <gsqwow@gmail.com>
2023-02-23 03:25:22 -0700

    Add Popup component

---
+++ src/components/Popup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Popup = props => {
+    return (
+        <div className='popup-div'>
+            You {props.gameState}!
+        </div>
+    )
+}
+
+export default Popup;
